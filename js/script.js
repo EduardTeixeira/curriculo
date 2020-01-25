a97cb54e551f81e973d16e988a8f140464dc21a8
eduard_teixeira <eduard.teixeira@etec.sp.gov.br>
2020-01-25 18:21:36 -0300

    add js

---
+++ js/script.js
@@ -0,0 +1,10 @@
+function calculateAge(day, month, year) {
+
+    var date = new Date;
+
+    console.log(date)
+    console.log("DIA :::: " + date.getDate())
+    console.log("MES :::: " + date.getMonth() + 1)
+    console.log("ANO :::: " + date.getFullYear())
+
+}

3b6d3161544d8941986a6812f6a8ad75b85f0992
eduard_teixeira <eduard.teixeira@etec.sp.gov.br>
2020-02-16 17:07:27 -0300

    style html and date diff

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
 function calculateAge(day, month, year) {
 
+    var birthday = new Date(year, month, day);
+
+    console.log("birthday")
+    console.log(birthday)
+
     var date = new Date;
 
     console.log(date)
@@ -7,4 +12,10 @@ function calculateAge(day, month, year) {
     console.log("MES :::: " + date.getMonth() + 1)
     console.log("ANO :::: " + date.getFullYear())
 
+    var diffDate = date - birthday;
+
+    diffDate = new Date(diffDate)
+
+    console.log('diff')
+    console.log(diffDate)
 }
